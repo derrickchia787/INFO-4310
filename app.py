9511c3823a2971b7356e6ed7ebe217bbf1eb9590
derrickchia787 <147205120+derrickchia787@users.noreply.github.com>
2025-02-06 21:30:05 -0500

    changes to app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 app.wsgi_app = WhiteNoise(app.wsgi_app, root="hw1/", prefix="hw1/", index_file="index.html", autorefresh=True)
 
 # You can change the prefix up above and get rid of this route if you just want to host static (which begs the question why you don't use something simpler like Github Pages)
-@app.route('/', methods=['GET'])
-def hello():
-  return make_response("Hello, world!!!!!!!!!!!!!!!!!!!!!!1")
+# @app.route('/', methods=['GET'])
+# def hello():
+#   return make_response("Hello, world!!!!!!!!!!!!!!!!!!!!!!1")
 
 if __name__ == "__main__":
   app.run(threaded=True, port=9000)
